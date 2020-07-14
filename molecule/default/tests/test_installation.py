5df2598c6f35c1e5a3a5ab224649a2e84299cbaf
Alexandre Chaussier <a.chaussier@infopen.pro>
2020-07-14 12:20:49 +0200

    Apply Infopen Ansible role template 0.41.1

--- molecule/default/tests/test_installation.py
+++ molecule/default/tests/test_installation.py
@@ -3,75 +3,61 @@ Role tests
 """
 
 import os
-import pytest
 from testinfra.utils.ansible_runner import AnsibleRunner
 
 testinfra_hosts = AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+PHP_VERSIONS = {
+    'buster': '7.3',
+    'stretch': '7.0',
+    'focal': '7.4',
+    'bionic': '7.2',
+    'xenial': '7.0',
+}
 
-@pytest.mark.parametrize('codename,package_name', [
-    ('trusty', 'php5'),
-    ('xenial', 'php'),
-])
-def test_packages(host, codename, package_name):
+
+def test_packages(host):
     """
     Ensure package installed
     """
 
-    if host.system_info.codename != codename:
-        pytest.skip('This element not apply on this codename')
-
-    assert host.package(package_name).is_installed
+    assert host.package('php').is_installed
 
 
-@pytest.mark.parametrize('codename,path', [
-    ('trusty', '/etc/php5/apache2/php.ini'),
-    ('trusty', '/etc/php5/cli/php.ini'),
-    ('xenial', '/etc/php/7.0/fpm/php.ini'),
-    ('xenial', '/etc/php/7.0/cli/php.ini'),
-])
-def test_config_files(host, codename, path):
+def test_config_files(host):
     """
     Ensure configuration files exists
     """
 
-    if host.system_info.codename != codename:
-        pytest.skip('This element not apply on this codename')
+    _version = PHP_VERSIONS[host.system_info.codename]
 
-    cfg_file = host.file(path)
-    assert cfg_file.exists
-    assert cfg_file.is_file
+    assert host.file('/etc/php/{}/fpm/php.ini'.format(_version)).exists
+    assert host.file('/etc/php/{}/fpm/php.ini'.format(_version)).is_file
+
+    assert host.file('/etc/php/{}/cli/php.ini'.format(_version)).exists
+    assert host.file('/etc/php/{}/cli/php.ini'.format(_version)).is_file
 
 
-@pytest.mark.parametrize('codename,path', [
-    ('trusty', '/etc/php5/cli/php.ini'),
-    ('xenial', '/etc/php/7.0/cli/php.ini'),
-])
-def test_config_files_content(host, codename, path):
+def test_config_files_content(host):
     """
     Test configuration file content
     """
 
-    if host.system_info.codename != codename:
-        pytest.skip('This element not apply on this codename')
+    _version = PHP_VERSIONS[host.system_info.codename]
 
-    cfg_file = host.file(path)
+    cfg_file = host.file('/etc/php/{}/cli/php.ini'.format(_version))
     assert cfg_file.contains('precision = 15')
-    assert cfg_file.contains('\[sqlite\]') is False
-    assert cfg_file.contains('\[sqlite3\]') is False
+    assert cfg_file.contains(r'\[sqlite\]') is False
+    assert cfg_file.contains(r'\[sqlite3\]') is False
 
 
-@pytest.mark.parametrize('codename,path', [
-    ('trusty', '/etc/php5/cli/conf.d/20-json.ini'),
-    ('xenial', '/etc/php/7.0/cli/conf.d/20-json.ini'),
-])
-def test_modules(host, codename, path):
+def test_modules(host):
     """
     Ensure JSON module is disabled
     """
 
-    if host.system_info.codename != codename:
-        pytest.skip('This element not apply on this codename')
+    _version = PHP_VERSIONS[host.system_info.codename]
 
-    assert host.file(path).exists is False
+    cfg_file = host.file('/etc/php/{}/cli/conf.d/20-json.ini'.format(_version))
+    assert cfg_file.exists is False
